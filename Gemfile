e7a8cac2ff1e1ef588eb2f867e71336783ff48c4
Marie <marie@violetco.de>
2015-04-06 18:30:13 -0700

    redo

---
+++ Gemfile
@@ -0,0 +1,7 @@
+source 'https://rubygems.org'
+ruby '2.2.0'
+
+gem 'jekyll'
+gem 'jekyll-sitemap'
+gem 'RedCloth'
+gem 'kramdown'
---
+++ dist/js/violetcode.js
@@ -0,0 +1,9 @@
+/*!
+ * VioletCode v0.1.8 (Built with UnderTasker)
+ */
+
+if (typeof jQuery === 'undefined') { throw new Error('UnderTasker\'s JavaScript requires jQuery') }
+
+/*!
+ * Put things here.
+ */

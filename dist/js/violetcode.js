33e5d5512c3aea3ff0f45f87540fd848de2f8f95
Marie <marie@violetco.de>
2015-04-24 13:11:01 -0700

    stuff

--- dist/js/violetcode.js
+++ dist/js/violetcode.js
@@ -7,18 +7,3 @@ if (typeof jQuery === 'undefined') { throw new Error('UnderTasker\'s JavaScript 
 /*!
  * Put things here.
  */
-
-$(function() {
-
-	$("#slideshow > div:gt(0)").hide();
-
-	setInterval(function() {
-	  $('#slideshow > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#slideshow');
-	},  3000);
-
-});
--- source/js/app.js
+++ source/js/app.js
@@ -1,18 +1,3 @@
 /*!
  * Put things here.
  */
-
-$(function() {
-
-	$("#slideshow > div:gt(0)").hide();
-
-	setInterval(function() {
-	  $('#slideshow > div:first')
-	    .fadeOut(1000)
-	    .next()
-	    .fadeIn(1000)
-	    .end()
-	    .appendTo('#slideshow');
-	},  3000);
-
-});
